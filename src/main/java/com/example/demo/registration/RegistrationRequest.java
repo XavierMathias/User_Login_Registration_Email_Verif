20fe067e79cd60b57ae19beefe18cbf350e1b323
David Santos <dsdsantos973@gmail.com>
2022-09-22 16:10:38 -0400

    added three more classes to register a new user

---
+++ src/main/java/com/example/demo/registration/RegistrationRequest.java
@@ -0,0 +1,22 @@
+/***
+ * Place where the user sends their request to register and be managed
+ */
+package com.example.demo.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class RegistrationRequest {
+
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+
+}
