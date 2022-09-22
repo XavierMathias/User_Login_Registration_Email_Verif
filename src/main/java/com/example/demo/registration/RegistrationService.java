fccca9d13d644c2cf15fa1671cae8aa2ba3e0d5b
David Santos <dsdsantos973@gmail.com>
2022-09-22 18:08:32 -0400

    worked creating secure endpoints

--- src/main/java/com/example/demo/registration/RegistrationService.java
+++ src/main/java/com/example/demo/registration/RegistrationService.java
@@ -2,6 +2,6 @@ package com.example.demo.registration;
 
 public class RegistrationService {
     public String register(RegistrationRequest request) {
-        return null;
+        return "works";
     }
 }
